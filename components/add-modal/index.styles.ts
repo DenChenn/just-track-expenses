ceb524b7156f198cd3855dc0a23ef8b76f1a7e3f
SwarzChen <aaaa102234@gmail.com>
2021-08-04 02:19:00 +0800

    :fire: finish selector

--- components/add-modal/index.styles.ts
+++ components/add-modal/index.styles.ts
@@ -4,14 +4,36 @@ export default StyleSheet.create({
   addModalContainer: {
     flexDirection: 'column',
     backgroundColor: '#ffffff',
-    height: '100%',
+    flex: 1,
+    alignItems: 'center',
   },
   iconContainer: {
+    position: 'relative',
     flexDirection: 'row',
     marginTop: 50,
     paddingLeft: 30,
+    flex: 1,
+  },
+  inputContainer: {
+    position: 'relative',
+    flex: 12,
+    alignItems: 'center',
+  },
+  inOutPickerContainer: {
+    flex: 0.5,
+
+    width: '80%',
+    marginBottom: 120,
+  },
+  typePickerContainer: {
+    flex: 1.5,
+
+    width: '80%',
+    marginBottom: 180,
   },
   input: {
+    position: 'relative',
+    width: 300,
     height: 60,
     borderRadius: 20,
     borderWidth: 1,
@@ -22,8 +44,25 @@ export default StyleSheet.create({
     marginHorizontal: 20,
     paddingLeft: 10,
     backgroundColor: '#ffffff',
+    flex: 1.4,
   },
-  buttonContainer: {
-    marginTop: 50,
+  buttonContainer: { position: 'relative', flex: 2 },
+  titleContainer: {
+    flex: 1,
+    marginTop: 40,
+    height: 50,
+    width: '100%',
+    fontSize: 24,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlignVertical: 'center',
+  },
+  titleText: {
+    width: '100%',
+    height: '100%',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+    fontSize: 24,
   },
 })
--- components/add-modal/index.tsx
+++ components/add-modal/index.tsx
@@ -7,9 +7,17 @@ import {
   Button,
   NativeSyntheticEvent,
   NativeTouchEvent,
+  Text,
 } from 'react-native'
 import { Formik } from 'formik'
 import Icon from 'react-native-vector-icons/AntDesign'
+import { Picker } from '@react-native-picker/picker'
+import { useState } from 'react'
+
+interface SubmitObject {
+  title: string
+  amount: string
+}
 
 const AddModal = (props: {
   modalOpen: boolean
@@ -18,82 +26,115 @@ const AddModal = (props: {
   setDateBillMap: React.Dispatch<React.SetStateAction<Map<string, BillData[]>>>
   currentDate: string
 }) => {
-  return (
-    <Modal visible={props.modalOpen} animationType="slide">
-      <View style={styles.addModalContainer}>
-        <View style={styles.iconContainer}>
-          <Icon
-            name="close"
-            size={24}
-            color="#000000"
-            onPress={() => props.setModalOpen(false)}
-          ></Icon>
-        </View>
-        <Formik
-          initialValues={{ title: '', billType: '', amount: '' }}
-          onSubmit={(values) => {
-            let newBillData: BillData = {
-              title: values.title,
-              amount: Number(values.amount),
-              billType: values.billType,
-            }
+  const handleSubmit = (values: SubmitObject) => {
+    let newBillData: BillData = {
+      title: values.title,
+      amount: Number(values.amount),
+      billType: selectedType,
+      inOrOut: selectedInOrOut,
+    }
 
-            if (props.dateBillMap.has(props.currentDate)) {
-              let prevBillData = props.dateBillMap.get(props.currentDate)
-              prevBillData?.push(newBillData)
-              props.setDateBillMap((prev) =>
-                prevBillData
-                  ? new Map([...prev, [props.currentDate, prevBillData]])
-                  : prev,
-              )
-            } else {
-              props.setDateBillMap(
-                (prev) =>
-                  new Map([...prev, [props.currentDate, [newBillData]]]),
-              )
-            }
-            props.setModalOpen(false)
-          }}
-        >
-          {(formikProps) => (
-            <View>
-              <TextInput
-                placeholder="Enter what you spent: "
-                onChangeText={formikProps.handleChange('title')}
-                value={formikProps.values.title}
-                style={styles.input}
-                placeholderTextColor="#ACBAC4"
-              />
-              <TextInput
-                placeholder="Enter the type you spent:"
-                onChangeText={formikProps.handleChange('billType')}
-                value={formikProps.values.billType}
-                style={styles.input}
-                placeholderTextColor="#ACBAC4"
-              />
-              <TextInput
-                placeholder="Enter how nuch you spent: "
-                onChangeText={formikProps.handleChange('amount')}
-                value={formikProps.values.amount}
-                style={styles.input}
-                keyboardType="numeric"
-                placeholderTextColor="#ACBAC4"
-              />
-              <View style={styles.buttonContainer}>
-                <Button
-                  title="Finish"
-                  color="#000000"
-                  onPress={
-                    (formikProps.handleSubmit as unknown) as (
-                      ev: NativeSyntheticEvent<NativeTouchEvent>,
-                    ) => void
-                  }
-                ></Button>
-              </View>
-            </View>
-          )}
-        </Formik>
+    if (props.dateBillMap.has(props.currentDate)) {
+      let prevBillData = props.dateBillMap.get(props.currentDate)
+      prevBillData?.push(newBillData)
+      props.setDateBillMap((prev) =>
+        prevBillData
+          ? new Map([...prev, [props.currentDate, prevBillData]])
+          : prev,
+      )
+    } else {
+      props.setDateBillMap(
+        (prev) => new Map([...prev, [props.currentDate, [newBillData]]]),
+      )
+    }
+    props.setModalOpen(false)
+  }
+  const [selectedType, setSelectedType] = useState('Food')
+  const [selectedInOrOut, setSelectedInOrOut] = useState('Income')
+
+  return (
+    <Modal
+      visible={props.modalOpen}
+      animationType="slide"
+      style={styles.addModalContainer}
+    >
+      <View style={styles.iconContainer}>
+        <Icon
+          name="close"
+          size={30}
+          color="#000000"
+          onPress={() => props.setModalOpen(false)}
+        ></Icon>
       </View>
+      <Formik initialValues={{ title: '', amount: '' }} onSubmit={handleSubmit}>
+        {(formikProps) => (
+          <View style={styles.inputContainer}>
+            <TextInput
+              placeholder="Enter what you spent: "
+              onChangeText={formikProps.handleChange('title')}
+              value={formikProps.values.title}
+              style={styles.input}
+              placeholderTextColor="#ACBAC4"
+            />
+            <TextInput
+              placeholder="Enter how nuch you spent: "
+              onChangeText={formikProps.handleChange('amount')}
+              value={formikProps.values.amount}
+              style={styles.input}
+              keyboardType="numeric"
+              placeholderTextColor="#ACBAC4"
+            />
+            <View style={styles.titleContainer}>
+              <Text style={styles.titleText}>Choose Spending Type</Text>
+            </View>
+            <View style={styles.inOutPickerContainer}>
+              <Picker
+                selectedValue={selectedInOrOut}
+                style={{
+                  position: 'relative',
+                  height: '100%',
+                  width: '100%',
+                }}
+                onValueChange={(itemValue) => setSelectedInOrOut(itemValue)}
+              >
+                <Picker.Item label="Income" value="income" />
+                <Picker.Item label="Expenses" value="expenses" />
+              </Picker>
+            </View>
+            <View style={styles.titleContainer}>
+              <Text style={styles.titleText}>Choose Spending Type</Text>
+            </View>
+            <View style={styles.typePickerContainer}>
+              <Picker
+                selectedValue={selectedType}
+                style={{
+                  position: 'relative',
+                  height: '100%',
+                  width: '100%',
+                }}
+                onValueChange={(itemValue) => setSelectedType(itemValue)}
+              >
+                <Picker.Item label="Food" value="cake" />
+                <Picker.Item label="Shopping" value="shopping-cart" />
+                <Picker.Item label="House" value="home" />
+                <Picker.Item label="Transportation" value="gauge" />
+                <Picker.Item label="Entertainment" value="game-controller" />
+              </Picker>
+            </View>
+            <View style={styles.buttonContainer}>
+              <Button
+                title="Finish"
+                color="#000000"
+                onPress={
+                  (formikProps.handleSubmit as unknown) as (
+                    ev: NativeSyntheticEvent<NativeTouchEvent>,
+                  ) => void
+                }
+              ></Button>
+            </View>
+          </View>
+        )}
+      </Formik>
     </Modal>
   )
 }
