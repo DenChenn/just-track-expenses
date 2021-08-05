2f74ac34920c0b7ba2b59c74149a5b697229d1a8
SwarzChen <aaaa102234@gmail.com>
2021-08-05 18:37:22 +0800

    :sparkles: add new bill container

--- components/bill/index.styles.ts
+++ components/bill/index.styles.ts
@@ -1,44 +1,16 @@
 import { StyleSheet } from 'react-native'
+import { HEIGHT } from '../bill-layout'
 
 export default StyleSheet.create({
-  container: {
-    backgroundColor: '#FFFFFF',
+  billConatiner: {
+    overflow: 'hidden',
     flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    shadowColor: '#ACBAC3',
-    shadowRadius: 10,
-    shadowOpacity: 0.6,
-    borderRadius: 15,
-    height: 80,
-    marginHorizontal: 20,
-    marginTop: 20,
+    justifyContent: 'flex-end',
   },
-  leftContainer: {
+  background: {
     flexDirection: 'row',
-    width: '70%',
-  },
-  rightContainer: {
-    flexDirection: 'row-reverse',
-    width: '30%',
-  },
-  iconContainer: {
-    width: '20%',
-    flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center',
-  },
-  titleContainer: {
-    fontSize: 24,
-    width: '80%',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  amountContainer: {
-    padding: 10,
-    fontSize: 20,
-    color: '#000000',
+    overflow: 'hidden',
   },
 })
--- components/bill/index.tsx
+++ components/bill/index.tsx
@@ -1,36 +1,94 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import BillLayout, { HEIGHT } from '../bill-layout'
+import SVGAction from '../svg-action'
+import { Dimensions, View } from 'react-native'
+import {
+  PanGestureHandler,
+  State,
+  TouchableWithoutFeedback,
+} from 'react-native-gesture-handler'
+import {
+  snapPoint,
+  timing,
+  useClock,
+  usePanGestureHandler,
+  useValue,
+  minus,
+  clamp,
+} from 'react-native-redash/lib/module/v1'
+import Animated, {
+  abs,
+  add,
+  call,
+  clockRunning,
+  cond,
+  eq,
+  min,
+  not,
+  set,
+  useCode,
+} from 'react-native-reanimated'
 import styles from './index.styles'
-import Icon from 'react-native-vector-icons/FontAwesome'
 
 const Bill = (props: {
   title: string
   amount: number
   inOrOut: string
   billType: string
+  onSwipe: () => void
 }) => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.leftContainer}>
-        <View style={styles.iconContainer}>
-          <Icon name={props.billType} color="#000000" size={24}></Icon>
-        </View>
+  const { width } = Dimensions.get('window')
+  const snapPointSet = [-width, -100, 0]
 
-        <Text style={styles.titleContainer}>{props.title}</Text>
+  const {
+    gestureHandler,
+    translation,
+    velocity,
+    state,
+  } = usePanGestureHandler()
+  const translateX = useValue(0)
+  const offsetX = useValue(0)
+  const height = useValue(HEIGHT + 40)
+  const clock = useClock()
+  const to = snapPoint(translateX, velocity.x, snapPointSet)
+  const shouldRemove = useValue(0)
+  useCode(
+    () => [
+      cond(
+        eq(state, State.ACTIVE),
+        set(translateX, add(offsetX, min(translation.x, 0))),
+      ),
+      cond(eq(state, State.END), [
+        set(translateX, timing({ clock, from: translateX, to })),
+        set(offsetX, translateX),
+        cond(eq(to, -width), set(shouldRemove, 1)),
+      ]),
+      cond(shouldRemove, [
+        set(height, timing({ from: HEIGHT + 40, to: 0 })),
+        cond(not(clockRunning(clock)), call([], props.onSwipe)),
+      ]),
+    ],
+    [props.onSwipe],
+  )
+
+  return (
+    <Animated.View style={[{ height }, styles.billConatiner]}>
+      <PanGestureHandler {...gestureHandler}>
+        <Animated.View style={{ transform: [{ translateX }] }}>
+          <BillLayout
+            title={props.title}
+            amount={props.amount}
+            inOrOut={props.inOrOut}
+            billType={props.billType}
+          ></BillLayout>
+        </Animated.View>
+      </PanGestureHandler>
+      <View style={styles.background}>
+        <TouchableWithoutFeedback onPress={() => shouldRemove.setValue(1)}>
+          <SVGAction x={abs(translateX)}></SVGAction>
+        </TouchableWithoutFeedback>
       </View>
-      <View style={styles.rightContainer}>
-        <Text
-          style={[
-            styles.amountContainer,
-            {
-              color: props.inOrOut === 'income' ? 'green' : 'red',
-            },
-          ]}
-        >
-          {props.amount.toString()}
-        </Text>
-      </View>
-    </View>
+    </Animated.View>
   )
 }
 
